b6099d5c2e0430121dd61b688e15a81c3d0cd12a
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2020-04-01 14:55:27 +0200

    add watermarker

--- pdf.py
+++ pdf.py
@@ -58,14 +58,47 @@ def merge(file_path_list):
     for item in file_path_list:
         extract(item)
         
+def watermark(args):
+    global pdf_writer
+    from reportlab.pdfgen import canvas
+    
+    if isfile(args[0]):
+        path = args[0]
+        watermark = args[1]
+    else:
+        logging.warning("First argument is not a path to file: will use it as watermark")
+        if not isfile(args[1]):
+            logging.error("Second argument should then be a path to file. Aborting")
+            raise FileNotFoundError
+        else:
+            path = args[1]
+            watermark = args[0]
+    logging.warning("Path to file: {}; watermark: {}".format(path,watermark))
+    c = canvas.Canvas("watermark.pdf")
+    c.setFontSize(22)
+    c.setFont('Helvetica-Bold', 36)
+    c.drawString(15, 15, watermark)
+    c.save()
+    
+    #with open("watermark.pdf", "rb") as waterfile:
+    watermark_object = PdfFileReader("watermark.pdf")
+        
+    pdf_reader = PdfFileReader(path)
+    page_count = pdf_reader.getNumPages()
+    for page_number in range(page_count):
+        input_page = pdf_reader.getPage(page_number)
+        input_page.mergePage(watermark_object.getPage(0))
+        pdf_writer.addPage(input_page)
 
 def main(args):
     global pdf_writer
     pdf_writer = PdfFileWriter()
     if not args["merge"] is None:
         merge(args["merge"])
-    else:
+    elif not args["extract"] is None:
         extract(args["extract"])
+    else:
+        watermark(args["watermark"])
 
     logging.info("Writing output to {}".format(args["destination"]))
     with open(args["destination"], "wb") as output:
@@ -79,5 +112,6 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-m", "--merge", nargs="+", help="List of paths to pdf files to merge together (plus additional page subset) in the provided order;")
     group.add_argument("-e", "--extract", help="Path to pdf file, and page subset, that have to be extracted.")
+    group.add_argument("-w", "--watermark", nargs=2, help="Watermark your pdf file.")
 
     main(vars(parser.parse_args()))
